095672469b915be484af90815a77df62c9552e96
Edwin Baranov <edwin.baranov@gmail.com>
2021-09-20 17:34:06 -0700

    Added incomplete parsJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -2,6 +2,39 @@
 // var parseJSON = JSON.parse;
 
 // but you're not, so you'll write it from scratch:
+
+/*[true, false]
+'true'
+'{"result":true,"count":42}'
+
 var parseJSON = function(json) {
-  // your code goes here
-};
+    //Array
+
+
+  //Objects
+  if (json[0] === '{') {
+    var result = {};
+    var endIndex = 0;
+
+    parseJSON(json.substring(1, ))
+
+  }
+
+
+  //String
+  if(json[0] === '"') {
+    return json.substring(1,json.length-1);
+  }
+
+  // boolean
+  if (json === 'true') {
+    return true;
+  }
+
+  if (json === 'false') {
+    return false;
+  }
+
+  //Everthing else
+  return parseFloat(json);
+};*/
